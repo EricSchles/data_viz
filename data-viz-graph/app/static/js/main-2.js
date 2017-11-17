530a43bc0ea798517dade726cba03d46d8d321af
Eric Schles <eric@elucd.com>
2017-11-17 10:17:59 -0500

    adding dynamic content

--- data-viz-graph/app/static/js/main-2.js
+++ data-viz-graph/app/static/js/main-2.js
@@ -4,15 +4,14 @@
 function buildGraph(response) {
   const cop = response.cop;
   const cops = response.cops;
+  const timestamp = response.timestamp;
 
 
   var chart = c3.generate({
       data: {
           x: 'x',
           columns: [
-              ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
-              // ['cop', 30, 200, 100, 400, 150, 250],
-              // ['cops', 130, 340, 200, 500, 250, 350]
+              timestamp,
               cop,
               cops
           ]
